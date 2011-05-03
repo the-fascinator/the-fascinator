2bb2ffb7f9fa558561dc95fab07a6629976ba80d
Oliver Lucido <dev@redboxresearchdata.com.au>
2011-05-03 02:31:05 +0000

    fixed bug with anotar and multi valued request params from jquery

--- portal/src/main/config/portal/default/default/scripts/actions/anotar.py
+++ portal/src/main/config/portal/default/default/scripts/actions/anotar.py
@@ -25,7 +25,9 @@ class AnotarData:
         self.rootUri = self.fd("rootUri")
         self.json = self.fd("json")
         self.type = self.fd("type")
-        self.rootUriList = self.vc("formData").getValues("rootUriList[]")
+        self.rootUriList = self.vc("formData").getValues("rootUriList")
+        if self.rootUriList is None:
+            self.rootUriList = self.vc("formData").getValues("rootUriList[]")
         self.portalPath = self.vc("portalPath")
         #print "action:'%s' formData:'%s'" % (self.action, formData)
 
