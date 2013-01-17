de5503b1fd8a766fe2643ca66ac7f3ab941671dc
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2013-01-17 16:46:14 +1030

    Added validation to HtmlFieldElement

--- portal/src/main/java/com/googlecode/fascinator/portal/workflow/components/HtmlFieldElement.java
+++ portal/src/main/java/com/googlecode/fascinator/portal/workflow/components/HtmlFieldElement.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class HtmlFieldElement implements HtmlComponent {
 
     private String componentTemplateName = null;
+    private Object[] validation = null;
 
     private Map<String, Object> parameterMap = new HashMap<String, Object>();
 
@@ -17,12 +18,24 @@ public class HtmlFieldElement implements HtmlComponent {
         this.componentTemplateName = componentTemplateName;
     }
 
+    public Object[] getValidation() {
+        return validation;
+    }
+
+    public void setValidation(Object[] validation) {
+        this.validation = validation;
+    }
+
     public Map<String, Object> getParameterMap() {
         return parameterMap;
     }
 
     public void setParameterMap(Map<String, Object> parameterMap) {
         this.parameterMap = parameterMap;
+    }
+
+    public boolean hasValidation() {
+        return validation != null;
     }
 
 }
