bab23a87d42448ce1fd5f57ff66cef317a1bc7d8
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2013-01-24 13:59:04 +1030

    Revert "Merge branch 'master' of https://github.com/the-fascinator/the-fascinator"
    This reverts commit 9709c6bbb8f4ec04243fca3ae31c0df2cae66f20, reversing
    changes made to e23eb4d34961e8951061eb499ee3d4a1121b9b3a.

--- portal/src/main/config/portal/default/default/scripts/layout.py
+++ portal/src/main/config/portal/default/default/scripts/layout.py
@@ -82,10 +82,6 @@ class LayoutData:
     def getSsoProviders(self):
         return self.security.ssoBuildLogonInterface(self.sessionState)
 
-    def getPackageTypes(self):
-        
-        return ''
-    
     def csrfSecurePage(self):
         pageName = self.vc("pageName");
         # Allow only POSTS to CSRF protected pages
