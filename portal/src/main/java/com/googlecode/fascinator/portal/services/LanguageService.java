88e9055ecd6dd6cf42f8eb55b09343bf185cad3e
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2013-02-26 16:32:31 +1030

    Added LanguageService that can be used to externalise strings from velocity templates etc

---
+++ portal/src/main/java/com/googlecode/fascinator/portal/services/LanguageService.java
@@ -0,0 +1,7 @@
+package com.googlecode.fascinator.portal.services;
+
+public interface LanguageService extends FascinatorService {
+
+    public abstract String displayMessage(String messageCode);
+
+}
