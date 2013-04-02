cfb58b4d5c14a9d77459626cd20c8070511054a1
Shilo Banihit <shiloworks@gmail.com>
2013-04-02 13:05:49 +1000

    DMPT - added lookup by country identifier(s)

--- portal/src/main/java/com/googlecode/fascinator/portal/api/impl/CountryLookupAPICallHandlerImpl.java
+++ portal/src/main/java/com/googlecode/fascinator/portal/api/impl/CountryLookupAPICallHandlerImpl.java
@@ -21,6 +21,8 @@ public class CountryLookupAPICallHandlerImpl implements APICallHandler {
     private ScriptingServices scriptingServices;
     private Logger log = LoggerFactory
             .getLogger(CountryLookupAPICallHandlerImpl.class);
+    private static int MAX_ROW_COUNT = 1000;
+    private static int MAX_PAGE_COUNT = 1000; 
     
     public CountryLookupAPICallHandlerImpl() {
 
@@ -28,17 +30,52 @@ public class CountryLookupAPICallHandlerImpl implements APICallHandler {
 
     @Override
     public String handleRequest(Request request) throws Exception {
-        Indexer indexer = scriptingServices.getIndexer();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        SearchRequest req = new SearchRequest("repository_name:\"Countries\"");
+        Indexer indexer = scriptingServices.getIndexer();        
+        String func = request.getParameter("func");
+        if ("detail".equals(func)) {
+        	return getDetails(request, indexer);
+        }   
+        return listAll(indexer);
+    }
+    
+    private String getDetails(Request request, Indexer indexer) throws Exception {    	
+    	String id = request.getParameter("id");
+    	if (id == null || "".equals(id)) {
+    		return "Invalid identifier";
+    	}
+    	id = id.replace(":","\\:");
+    	if (id.indexOf(",") != -1) {
+    		String[] ids = id.split(",");
+    		StringBuilder strIds = new StringBuilder();
+    		for (String idEntry : ids) {
+    			if (strIds.length() > 0) {
+    				strIds.append(" OR ");
+    			}
+    			strIds.append(idEntry);
+    		}
+    		id = strIds.toString();
+    	} 
+    	SearchRequest req = new SearchRequest("repository_name:\"Countries\" AND (dc_identifier:(" + id + "))");	
         req.setParam("fl", "dc_title,dc_identifier");
-        req.setParam("rows", "1000");
+        req.setParam("rows", String.valueOf(MAX_ROW_COUNT));        
+        return getEntries(req, indexer);
+    }
+    
+    private String listAll(Indexer indexer) throws Exception {    	
+    	SearchRequest req = new SearchRequest("repository_name:\"Countries\"");
+        req.setParam("fl", "dc_title,dc_identifier");
+        req.setParam("rows", String.valueOf(MAX_ROW_COUNT));
         req.setParam("sort","dc_title asc");
-        indexer.search(req, out);
+        return getEntries(req, indexer);
+    }
+    
+    private String getEntries(SearchRequest req, Indexer indexer) throws Exception {
+    	ByteArrayOutputStream out = new ByteArrayOutputStream();
+    	indexer.search(req, out);
         SolrResult res = new SolrResult(new ByteArrayInputStream(out.toByteArray()));
         int numFound = res.getNumFound();
         int start = 0;
-        int pageSize = 1000;
+        int pageSize = MAX_PAGE_COUNT;
         StringBuilder strBuilder = new StringBuilder();
         strBuilder.append("[");
         strBuilder.append(System.getProperty("line.separator"));
@@ -72,6 +109,7 @@ public class CountryLookupAPICallHandlerImpl implements APICallHandler {
             indexer.search(req, out);
             res = new SolrResult(new ByteArrayInputStream(out.toByteArray()));
         }
+        strBuilder.append(System.getProperty("line.separator"));
         strBuilder.append("]");
         return strBuilder.toString();
     }
